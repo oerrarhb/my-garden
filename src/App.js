0898fc7e3d7832ab3371218673a1afe73269778e
Othmane ERRARHBI <othmane.errarhbi@deveryware.com>
2022-03-26 17:40:37 +0100

    test some components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Header()
+function Banner()
 {
-  return <h1>My Garden</h1>
+  const title = "My Garden";
+  return (<h1>{title}</h1>)
+}
+
+function Cart()
+{
+    const monsteraPrice = 8;
+    const ivyPrice = 10;
+    const flowerPrice = 15;
+
+    return (
+      <div>
+        <h2>Cart</h2>
+        <ul>
+          <li>Monstera : {monsteraPrice}</li> 
+          <li>Ivy : {ivyPrice}</li> 
+          <li>Flowers : {flowerPrice}</li> 
+        </ul>
+        Total price : {monsteraPrice + ivyPrice + flowerPrice}
+      </div>
+    );
 }
 
 
 function App() {
   return (
-    <Header/>
+    <div>
+      <Banner/>
+      <Cart/>
+    </div>
   );
 }
 
