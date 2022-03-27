d4f327cd91d7f9305cd2c333b6a4344e4f380a77
Othmane ERRARHBI <othmane.errarhbi@deveryware.com>
2022-03-27 11:01:47 +0200

    add cover & caring scale to plants

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,9 +4,11 @@ import logo from '../assets/logo.png'
 function Banner()
 {
   const title = "My Garden";
-  return (<div className="lmj-banner">
-      <img src = {logo} alt='My Garden' className='lmj-logo'/>
-      <h1 className="lmj-title">{title}</h1>
-      </div>);
+  return (
+		<div className='lmj-banner'>
+			<img src={logo} alt='La maison jungle' className='lmj-logo' />
+			<h1 className='lmj-title'>{title}</h1>
+		</div>
+	)
 }
 export default Banner
---
+++ src/data/plantList.js
@@ -0,0 +1,90 @@
+import monstera from '../assets/monstera.jpg';
+import ficuslyrata from '../assets/ficuslyrata.png';
+import pothos from '../assets/pothos.jpg';
+import yucca from '../assets/yucca.jpeg';
+import olivier from '../assets/olivier.webp';
+import basilic from '../assets/basilic.jpg';
+import succulente from '../assets/succulente.jpg';
+import geranium from '../assets/geranium.webp';
+import aloe from '../assets/aloe.jpg';
+
+
+
+
+
+export const plantList = [
+	{
+		name: 'monstera',
+		category: 'classique',
+		id: '1ed',
+		isBestSale: true,
+		light: 2,
+		water: 3,
+		cover: monstera
+	},
+	{
+		name: 'ficus lyrata',
+		category: 'classique',
+		id: '2ab',
+		light: 3,
+		water: 1,
+		cover: ficuslyrata
+	},
+	{
+		name: 'pothos argyraeus',
+		category: 'classique',
+		id: '3sd',
+		light: 1,
+		water: 2,
+		cover: pothos
+	},
+	{
+		name: 'yucca',
+		category: 'classique',
+		id: '4kk',
+		light: 3,
+		water: 1,
+		cover: yucca
+	},
+	{
+		name: 'olivier',
+		category: 'extérieur',
+		id: '5pl',
+		light: 3,
+		water: 1,
+		cover: olivier
+	},
+	{
+		name: 'géranium',
+		category: 'extérieur',
+		id: '6uo',
+		light: 2,
+		water: 2,
+		cover: geranium
+	},
+	{
+		name: 'basilic',
+		category: 'extérieur',
+		id: '7ie',
+		isBestSale: true,
+		light: 2,
+		water: 3,
+		cover: basilic
+	},
+	{
+		name: 'aloe vera',
+		category: 'plante grasse',
+		id: '8fp',
+		light: 2,
+		water: 1,
+		cover: aloe
+	},
+	{
+		name: 'succulente',
+		category: 'plante grasse',
+		id: '9vn',
+		light: 2,
+		water: 1,
+		cover: succulente
+	}
+]
